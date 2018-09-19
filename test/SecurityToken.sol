97c4362f1ab23a38d8dc990c6fb2b1ac89cde4ab
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2018-09-20 04:17:32 +1000

    Interfaces

--- test/SecurityToken.sol
+++ test/SecurityToken.sol
@@ -31,6 +31,76 @@ contract CanSendCodes {
     byte constant TRANSFER_BLOCKED_TOKEN_GRANULARITY = 0xA9;            // Transfer Blocked - Token granularity
 }
 
+/// @title ERC-1410 Partially Fungible Token Standard
+/// @dev See https://github.com/SecurityTokenStandard/EIP-Spec
+
+/*
+interface ERC777Token {
+    function name() public view returns (string);
+    function symbol() public view returns (string);
+    function totalSupply() public view returns (uint256);
+    function balanceOf(address owner) public view returns (uint256);
+    function granularity() public view returns (uint256);
+
+    function defaultOperators() public view returns (address[]);
+    function authorizeOperator(address operator) public;
+    function revokeOperator(address operator) public;
+    function isOperatorFor(address operator, address tokenHolder) public view returns (bool);
+
+    function send(address to, uint256 amount, bytes data) public;
+    function operatorSend(address from, address to, uint256 amount, bytes data, bytes operatorData) public;
+
+    function burn(uint256 amount, bytes data) public;
+    function operatorBurn(address from, uint256 amount, bytes data, bytes operatorData) public;
+
+    event Sent(
+        address indexed operator,
+        address indexed from,
+        address indexed to,
+        uint256 amount,
+        bytes data,
+        bytes operatorData
+    );
+    event Minted(address indexed operator, address indexed to, uint256 amount, bytes data, bytes operatorData);
+    event Burned(address indexed operator, address indexed from, uint256 amount, bytes operatorData);
+    event AuthorizedOperator(address indexed operator, address indexed tokenHolder);
+    event RevokedOperator(address indexed operator, address indexed tokenHolder);
+}
+*/
+/*
+interface IERC1410 is IERC777 {
+
+    function getDefaultTranches(address _tokenHolder) external view returns (bytes32[]);
+    function setDefaultTranche(bytes32[] _tranches) external;
+    function balanceOfByTranche(bytes32 _tranche, address _tokenHolder) external view returns (uint256);
+    function sendByTranche(bytes32 _tranche, address _to, uint256 _amount, bytes _data) external returns (bytes32);
+    function sendByTranches(bytes32[] _tranches, address[] _tos, uint256[] _amounts, bytes _data) external returns (bytes32[]);
+    function operatorSendByTranche(bytes32 _tranche, address _from, address _to, uint256 _amount, bytes _data, bytes _operatorData) external returns (bytes32);
+    function operatorSendByTranches(bytes32[] _tranches, address[] _froms, address[] _tos, uint256[] _amounts, bytes _data, bytes _operatorData) external returns (bytes32[]);
+    function tranchesOf(address _tokenHolder) external view returns (bytes32[]);
+    function defaultOperatorsByTranche(bytes32 _tranche) external view returns (address[]);
+    function authorizeOperatorByTranche(bytes32 _tranche, address _operator) external;
+    function revokeOperatorByTranche(bytes32 _tranche, address _operator) external;
+    function isOperatorForTranche(bytes32 _tranche, address _operator, address _tokenHolder) external view returns (bool);
+    function redeemByTranche(bytes32 _tranche, uint256 _amount, bytes _data) external;
+    function operatorRedeemByTranche(bytes32 _tranche, address _tokenHolder, uint256 _amount, bytes _operatorData) external;
+
+    event SentByTranche(
+        bytes32 indexed fromTranche,
+        bytes32 toTranche,
+        address indexed operator,
+        address indexed from,
+        address indexed to,
+        uint256 amount,
+        bytes data,
+        bytes operatorData
+    );
+    event AuthorizedOperatorByTranche(bytes32 indexed tranche, address indexed operator, address indexed tokenHolder);
+    event RevokedOperatorByTranche(bytes32 indexed tranche, address indexed operator, address indexed tokenHolder);
+    event BurnedByTranche(bytes32 indexed tranche, address indexed operator, address indexed from, uint256 amount, bytes operatorData);
+
+}
+*/
 /*
 interface IERCST is IERCPFT {
     function getDocument(bytes32 _name) external view returns (string _uri, bytes32 _documentHash);
